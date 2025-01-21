ce535f34e59fe28c6e8396426bf5ffd3ae5b0338
zakwanbdt25 <zakwanzafar946@gmail.com>
2025-01-21 08:56:35 +0530

    Add files via upload

---
+++ Nestedifelse.py
@@ -0,0 +1,9 @@
+i=10
+if(i==10):
+    if(i<20):
+        print("i is Smaller than 20")
+if(i < 15):
+    print("i is Smaller than 15 too")
+else:
+    print("i is Greater than 15") 
+
---
+++ Nestediflab.py
@@ -0,0 +1,11 @@
+letter="A"
+if letter=="B":
+  print("The letter is B")
+elif letter=="C":
+ print("The letter is C")
+elif letter=="A":
+      print("The Letter is A")
+else:
+     print("The Letter isn't A,B and C")
+
+
